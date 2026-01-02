3d2b8f7bc0b88bfb2ecffa50e560021b4272ba93
MaxKatafalk <max20060602@mail.ru>
2026-01-02 12:25:09 +1000

    Введены классы игрового поля и игровой клетки

---
+++ Saper/GameBoard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Minesweeper
+{
+    public class GameBoard
+    {
+        public GameCell[,] Cells;
+        public int Width = 16;
+        public int Height = 16;
+
+        public GameBoard()
+        {
+            CreateEmptyBoard();
+        }
+
+        private void CreateEmptyBoard()
+        {
+            Cells = new GameCell[Width, Height];
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    Cells[x, y] = new GameCell(x, y);
+                }
+            }
+        }
+
+        public GameCell GetCell(int x, int y)
+        {
+            if (x >= 0 && x < Width && y >= 0 && y < Height)
+            {
+                return Cells[x, y];
+            }
+            return null;
+        }
+
+        public void ResetBoard()
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    Cells[x, y].Reset();
+                }
+            }
+        }
+    }
+}
---
+++ Saper/GameCell.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Minesweeper
+{
+    public class GameCell
+    {
+        private bool hasMine;
+        private int minesAround;
+        private int state;
+
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public bool HasMine
+        {
+            get { return hasMine; }
+            set { hasMine = value; }
+        }
+
+        public int MinesAround
+        {
+            get { return minesAround; }
+            set { minesAround = value; }
+        }
+
+        public int State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+
+        public bool IsOpened()
+        {
+            return state == 1;
+        }
+
+        public bool IsFlagged()
+        {
+            return state == 2;
+        }
+
+        public bool HasQuestion()
+        {
+            return state == 3;
+        }
+
+        public bool IsClosed()
+        {
+            return state == 0;
+        }
+
+        public GameCell(int x, int y)
+        {
+            X = x;
+            Y = y;
+            hasMine = false;
+            minesAround = 0;
+            state = 0; 
+        }
+
+        public void Open()
+        {
+            if (state == 0) 
+            {
+                state = 1;
+            }
+        }
+
+        public void ToggleMark()
+        {
+            if (state == 0)
+            {
+                state = 2;
+            }
+            else if (state == 2)
+            {
+                state = 3;
+            }
+            else if (state == 3)
+            {
+                state = 0;
+            }
+        }
+
+        public void Reset()
+        {
+            hasMine = false;
+            minesAround = 0;
+            state = 0;
+        }
+    }
+}
